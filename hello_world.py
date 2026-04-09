271484a09a4cf39a61b590364e2eb9055d13611c
Liet1 <987julieta@gmail.com>
2026-04-08 21:54:32 -0600

    change1

--- hello_world.py
+++ hello_world.py
@@ -10,7 +10,7 @@ html_content = b"""
     <title>Hello World Frontend</title>
 </head>
 <body>
-    <h1>Hello, World!</h1>
+    <h1>Hello, World!test</h1>
     <p>Este es un ejemplo sencillo de frontend en Python.</p>
 </body>
 </html>
